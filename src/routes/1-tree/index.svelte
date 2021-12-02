4e6f15c4b61ade6104a966d1d277fd849b830a4e
casderooij <hello@casderooij.nl>
2021-12-02 11:15:32 +0100

    Add website page. Add layout file. Add global styling

---
+++ src/routes/1-tree/index.svelte
@@ -0,0 +1,7 @@
+<h1>Tree</h1>
+
+<style>
+  :global(body) {
+    background-color: yellow;
+  }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,8 @@
 <h1>Websites</h1>
+
+<style>
+  :global(body) {
+    padding: 10px;
+    color: red;
+  }
+</style>
